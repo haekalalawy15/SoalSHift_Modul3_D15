1e097501c605da4c600ed901e14eca0ae4855287
Rimba Azhara <rimbaazhara55@gmail.com>
2017-10-25 11:48:21 +0700

    player 2 sama show point

--- nomor2.c
+++ nomor2.c
@@ -12,6 +12,13 @@ int ranjau[2][17];
 int terisi;
 char* player1;
 char* player2;
+int status;
+
+
+void show_point(){
+	printf("%s : %d\n", &player1,poin1);
+	printf("%s : %d\n", &player2,poin2);
+}
 
 void* turn_player1(void *arg)
 {
@@ -64,6 +71,60 @@ void* turn_player1(void *arg)
 	status=2;
 	return NULL;
  
+}
+
+
+void* turn_player2(void *arg)
+{
+    
+    while(poin1<10&&poin2<10){
+    	while(status == 0)
+    	{
+
+    	}
+		if(status==2)return NULL;
+		terisi=0;
+    	for(int i=0;i<2;i++){
+    		for(int j=1;j<=16;j++){
+    			if(ranjau[i][j]==1)terisi++;
+    		}
+    	}
+    	if(terisi==32)break;
+	char c;
+	while(1){
+	    	printf("Show Point?(y/n)\n");
+    		scanf(" %c", &c);
+    		if(c=='y'){show_point();break;}
+		else break;
+    	}
+    	printf("%s, Isi Lubang?(Jika kurang dari 4, isi dengan 0)\n", &player2);
+    	for(int i=0;i<4;i++){
+    		int n;
+			scanf("%d", &n);
+			ranjau[1][n]=1;
+    	}
+    	
+   	system("clear");
+    	
+    	printf("%s, Tebak 4 Lubang\n", &player1);
+    	for(int i=0;i<4;i++){
+    		int n;
+			scanf("%d", &n);
+			if(ranjau[1][n]==1)poin2++;
+			else poin1++;
+    	}
+    	
+    	status=0;
+	}
+    printf("Permainan Berakhir\n");
+	show_point();
+	if(poin1>poin2)printf("%s menang\n", &player1);
+	else if(poin1<poin2)printf("%s menang\n", &player2);
+	else printf("Permainan Seri\n");
+	status=2;
+	return NULL;
+    
+    
 }
 
 int main()
